25d5223ae74d1fa9b10d4fe394f2a553037ca172
matins85 <olorunsholamatins@gmail.com>
2022-03-28 07:17:35 +0100

    update sidenav bar

--- src/app/public-layout/app-public-sidenav-list/app-public-sidenav-list.component.ts
+++ src/app/public-layout/app-public-sidenav-list/app-public-sidenav-list.component.ts
@@ -2,6 +2,15 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ToggleNavService } from '../sharedService/toggle-nav.service';
+// state management
+import { Store } from '@ngrx/store';
+import { Profile } from '../../models/irm';
+import { AppState, selectAllProfile } from 'src/app/reducers/index';
+import { AddProfile} from '../../actions/irm.action';
+import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-app-public-sidenav-list',
@@ -14,11 +23,13 @@ export class AppPublicSidenavListComponent implements OnInit {
   clickEventSubscription?: Subscription;
 
   panelOpenState = false;
+  profile: any;
   
-  
-  constructor(private shared: ToggleNavService,
-    private router: Router) { 
+  stateProfile: Observable<Profile[]>;
 
+  constructor(private router: Router, private authService: AuthService,
+    private httpService: HttpService, private store: Store<AppState>) { 
+      this.stateProfile = store.select(selectAllProfile);
   }
 
   routeRedirect() {
@@ -26,14 +37,65 @@ export class AppPublicSidenavListComponent implements OnInit {
   }
 
 
-  ngOnInit(): void {
+  getJwtToken(): any {
+    return this.authService.getJwtToken();
+  }
+
+
+  AddProfile() {
+    this.stateProfile.forEach(e => {
+      if(e.length > 0 ) {
+        this.profile = e[0].data.data;
+        console.log("profile_state",e[0].data.data)
+      }
+      else {
+        this.httpService.getProfile(this.getJwtToken()).subscribe(
+          (data:any) => {
+            if(data.responsecode == "01"){
+            }else{
+              this.store.dispatch(new AddProfile([{id: 1, data: data}]));
+              this.profile = data.data;
+              console.log("http_profile", data.data)
+            }
+          },
+          (err: any) => {
+            this.authService.refreshToken();
+          }
+        )
+      }
+    }) 
+  }
+
+
+  limit(title: any, limit = 11) {
+    if(title === undefined) {
+      return ''
+    }else {
+    const newTitle: any = [];
+    if(title.length > limit) {
+      title.split('').reduce((acc: any, cur: any) => {
+        if(acc + cur.length <= limit) {
+          newTitle.push(cur);
+        }
+        return acc + cur.length;
+      }, 0);
+      return `${newTitle.join('')}...`;
+    }
+    return title;
+  }
   }
 
 
   logout() {
-    this.shared.sendClickEvent();
+    this.authService.logout()
+    this.router.navigate([''])
   }
 
+
+  ngOnInit(): void {
+    this.AddProfile();
+  }
+  
 
   public onSidenavClose = () => {
     this.sidenavClose.emit();
