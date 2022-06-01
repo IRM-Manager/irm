8f11f08592f4dd768badda45d5c31be42cfff757
matins85 <olorunsholamatins@gmail.com>
2022-06-01 16:06:30 +0100

    commit

--- src/app/public-layout/payee-layout/payee-generate-bill/payee-generate-bill.module.ts
+++ src/app/public-layout/payee-layout/payee-generate-bill/payee-generate-bill.module.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { PayeeGenerateBillRoutingModule } from './payee-generate-bill-routing.module';
 import { PayeeGenerateBillComponent } from './payee-generate-bill.component';
+// module
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { DataTablesModule } from 'angular-datatables';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @NgModule({
@@ -11,7 +24,20 @@ import { PayeeGenerateBillComponent } from './payee-generate-bill.component';
   ],
   imports: [
     CommonModule,
-    PayeeGenerateBillRoutingModule
+    PayeeGenerateBillRoutingModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    DataTablesModule,
+    MatToolbarModule,
+    MatDatepickerModule,
+    NgxMatSelectSearchModule,
+    MatSelectModule,
+    MatMenuModule,
   ]
 })
 export class PayeeGenerateBillModule { }
--- src/app/public-layout/tax-payer/tax-payer.component.ts
+++ src/app/public-layout/tax-payer/tax-payer.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, OnDestroy, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { Subject } from 'rxjs';
@@ -7,8 +13,17 @@ import { DialogComponent } from '../dialog/dialog.component';
 // state management
 import { Store } from '@ngrx/store';
 import { IndPayer, ComPayer } from '../../models/irm';
-import { AppState, selectAllIndPayer, selectAllComPayer } from 'src/app/reducers/index';
-import { AddIndPayer, RemoveIndPayer, AddComPayer, RemoveComPayer } from '../../actions/irm.action';
+import {
+  AppState,
+  selectAllIndPayer,
+  selectAllComPayer,
+} from 'src/app/reducers/index';
+import {
+  AddIndPayer,
+  RemoveIndPayer,
+  AddComPayer,
+  RemoveComPayer,
+} from '../../actions/irm.action';
 import { Observable } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
@@ -20,11 +35,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   selector: 'app-tax-payer',
   templateUrl: './tax-payer.component.html',
   encapsulation: ViewEncapsulation.Emulated,
-  styleUrls: ['./tax-payer.component.scss']
+  styleUrls: ['./tax-payer.component.scss'],
 })
 export class TaxPayerComponent implements OnDestroy, OnInit {
-
-  
   search: string = '';
   active: any = 'ind';
   left_text!: string;
@@ -44,73 +57,91 @@ export class TaxPayerComponent implements OnDestroy, OnInit {
   private readonly REFRESH_TOKEN = BaseUrl.refresh_token;
   private helper = new JwtHelperService();
 
-  constructor(private router: Router, private direct: ActivatedRoute, private store: Store<AppState>,
-    private authService: AuthService, private httpService: HttpService, private dialog: MatDialog,
-    private snackBar: MatSnackBar, private fb: FormBuilder) {
+  constructor(
+    private router: Router,
+    private direct: ActivatedRoute,
+    private store: Store<AppState>,
+    private authService: AuthService,
+    private httpService: HttpService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder
+  ) {
+    this.authService.checkExpired();
 
-      this.authService.checkExpired()
-
-    this.direct.paramMap.subscribe(params => {
-      if (params.get('id') === '' || params.get('id') === undefined || params.get('id') === null) {
+    this.direct.paramMap.subscribe((params) => {
+      if (
+        params.get('id') === '' ||
+        params.get('id') === undefined ||
+        params.get('id') === null
+      ) {
         this.active = 'ind';
-        this.left_text = "Tax Registration of Individuals";
-      }
-      else if  (params.get('id') == 'non') {
+        this.left_text = 'Tax Registration of Individuals';
+      } else if (params.get('id') == 'non') {
         this.active = 'com';
-        this.left_text = "Tax Registration of Business";
-
-      }
-      else if  (params.get('id') == 'ind') {
+        this.left_text = 'Tax Registration of Business';
+      } else if (params.get('id') == 'ind') {
         this.active = 'ind';
-        this.left_text = "Tax Registration of Individuals";
-      }
-      else {
+        this.left_text = 'Tax Registration of Individuals';
+      } else {
         this.active = 'ind';
-        this.left_text = "Tax Registration of Individuals";
+        this.left_text = 'Tax Registration of Individuals';
       }
       this.renderTable();
-      
-    })
+    });
 
     this.stateIndPayer = store.select(selectAllIndPayer);
     this.stateComPayer = store.select(selectAllComPayer);
-
   }
-
 
   formatDate(data: any) {
     var d = new Date(data),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
-    if (month.length < 2) 
-        month = '0' + month;
-    if (day.length < 2) 
-        day = '0' + day;
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
     return [year, month, day].join('-');
   }
-
 
   modelChange(search: any) {
     if (this.active == 'com') {
       const data = this.searchData2?.filter((data: any) => {
-        return data.tin.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.company_payer[0].organisation_name.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.company_payer[0].org_phone.toLowerCase().startsWith(search.toLowerCase()) ||
-        this.formatDate(data.created_at).startsWith(search.toLowerCase())
-      })
+        return (
+          data.tin.toLowerCase().startsWith(search.toLowerCase()) ||
+          data.company_payer[0].organisation_name
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          data.company_payer[0].org_phone
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          this.formatDate(data.created_at).startsWith(search.toLowerCase())
+        );
+      });
       this.datas = data;
-    }else {
+    } else {
       const data = this.searchData?.filter((data: any) => {
-        return data.tin.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.individual_payer[0].profession_trade.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.individual_payer[0].phone.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.individual_payer[0].first_name.startsWith(search.toLowerCase()) ||
-        data.individual_payer[0].surname.toLowerCase().startsWith(search.toLowerCase()) ||
-        data.individual_payer[0].middle_name.toLowerCase().startsWith(search.toLowerCase()) ||
-        this.formatDate(data.created_at).startsWith(search.toLowerCase())
-      })
+        return (
+          data.tin.toLowerCase().startsWith(search.toLowerCase()) ||
+          data.individual_payer[0].profession_trade
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          data.individual_payer[0].phone
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          data.individual_payer[0].first_name.startsWith(
+            search.toLowerCase()
+          ) ||
+          data.individual_payer[0].surname
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          data.individual_payer[0].middle_name
+            .toLowerCase()
+            .startsWith(search.toLowerCase()) ||
+          this.formatDate(data.created_at).startsWith(search.toLowerCase())
+        );
+      });
       this.datas = data;
     }
   }
@@ -123,75 +154,70 @@ export class TaxPayerComponent implements OnDestroy, OnInit {
 
     if (this.active == 'com') {
       this.isLoading = true;
-      this.stateComPayer?.forEach(e => {
-        if(e.length > 0 ) {
+      this.stateComPayer?.forEach((e) => {
+        if (e.length > 0) {
           this.datas = e[0].data;
           this.searchData2 = e[0].data;
-          console.log(e[0].data)
-          this.dtTrigger.next
+          console.log(e[0].data);
+          this.dtTrigger.next;
           this.isLoading = false;
-        }
-        else {
+        } else {
           this.httpService.GetPayerList().subscribe(
-            (data:any) => {
-              console.log(data)
-                const company = data.filter((type: any) => {
-                  return type.payer_type == 'company'
-                })
-                this.store.dispatch(new AddComPayer([{id: 1, data: company}]));
-                this.datas = company;
-                this.searchData2 = company;
-                this.dtTrigger.next
-                this.isLoading = false;
+            (data: any) => {
+              console.log(data);
+              const company = data.filter((type: any) => {
+                return type.payer_type == 'company';
+              });
+              this.store.dispatch(new AddComPayer([{ id: 1, data: company }]));
+              this.datas = company;
+              this.searchData2 = company;
+              this.dtTrigger.next;
+              this.isLoading = false;
             },
-            err => {
+            (err) => {
               this.isLoading = false;
               this.authService.checkExpired();
             }
-          )
+          );
         }
-      }) 
-    }
-    else {
+      });
+    } else {
       this.isLoading = true;
-      this.stateIndPayer?.forEach(e => {
-        if(e.length > 0 ) {
+      this.stateIndPayer?.forEach((e) => {
+        if (e.length > 0) {
           this.datas = e[0].data;
           this.searchData = e[0].data;
-          console.log(e[0].data)
-          this.dtTrigger.next
+          console.log(e[0].data);
+          this.dtTrigger.next;
           this.isLoading = false;
-        }
-        else {
+        } else {
           this.httpService.GetPayerList().subscribe(
-            (data:any) => {
-              console.log(data)
+            (data: any) => {
+              console.log(data);
               const individual = data.filter((type: any) => {
-                return type.payer_type == 'individual'
-              })
-                this.store.dispatch(new AddIndPayer([{id: 1, data: individual}]));
-                this.datas = individual;
-                this.searchData = individual;
-                this.dtTrigger.next
-                this.isLoading = false;
+                return type.payer_type == 'individual';
+              });
+              this.store.dispatch(
+                new AddIndPayer([{ id: 1, data: individual }])
+              );
+              this.datas = individual;
+              this.searchData = individual;
+              this.dtTrigger.next;
+              this.isLoading = false;
             },
-            err => {
+            (err) => {
               this.isLoading = false;
             }
-          )
+          );
         }
-      }) 
+      });
     }
-
-
   }
-
 
   ngOnInit(): void {
     this.authService.checkExpired();
     this.renderTable();
   }
-
 
   Reload() {
     this.is_reload = true;
@@ -199,35 +225,30 @@ export class TaxPayerComponent implements OnDestroy, OnInit {
     this.is_reload = false;
   }
 
-
   changeActive(type: string) {
     this.active = type;
-    if (type=="com") {
-      this.left_text = "Tax Registration of Business";
+    if (type == 'com') {
+      this.left_text = 'Tax Registration of Business';
       this.router.navigate(['/dashboard2/taxpayer/non']);
-    }
-    else {
-      this.left_text = "Tax Registration of Individuals";
+    } else {
+      this.left_text = 'Tax Registration of Individuals';
       this.router.navigate(['/dashboard2/taxpayer/ind']);
     }
     this.renderTable();
   }
 
-
   OpenDialog(data: any, type: string) {
-      this.snackBar.dismiss()
-      this.dialog.open(DialogComponent, {
-        data: {
-          type: type,
-          data: data
-        }
-      });
+    this.snackBar.dismiss();
+    this.dialog.open(DialogComponent, {
+      data: {
+        type: type,
+        data: data,
+      },
+    });
   }
 
   ngOnDestroy(): void {
     // Do not forget to unsubscribe the event
     this.dtTrigger.unsubscribe();
   }
-
-
 }
